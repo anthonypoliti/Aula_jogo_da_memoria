51032bba8bad6ba587561a8ffd3f6287491d0e03
Anthony Politi <anthonypoliti220@gmail.com>
2025-09-05 18:53:11 -0300

    Adicionando o flip na cartas

--- src/scripts/cards.js
+++ src/scripts/cards.js
@@ -13,8 +13,30 @@ class CardManager {
     gen(Heronumber) {
         let templete = document.getElementById("cardTemplate") //Referenciar o template
         let clone = templete.content.cloneNode(true);        //Criar um clone
+
+        // Captura os cliques nas cartas
+        clone.children[0].addEventListener("click",
+            event => this.onClick(event)
+        )
         //retomar o clone
         return clone;
+    }
+    // Captura eventos de clique
+    onClick(event) {
+        this.flip(event.target);
+    }
+    // Vira a carta 
+    flip(cardNode) {
+        cardNode.children[0].classList.add("selected");
+    }
+    // Desvira a carta
+    unFlip(cardNode) {
+        cardNode.children[0].classList.remove("selected");
+    }
+    // Método que desabilita o movimento da carta que já foi virada
+    disable(cardNode) {
+        cardNode.children[0].classList.add("matched");
+        this.unFlip(cardNode);
     }
 
 }
